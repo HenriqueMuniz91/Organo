c314a0b59899df6893633e9d09597cd2e27f00fe
henrique <henriquecontact@gmail.com>
2024-03-19 19:33:37 -0300

    Add Filter in App.js

--- src/App.js
+++ src/App.js
@@ -53,15 +53,17 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
+      <Formulario 
+        times={times.map(time => time.nome)} 
+        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
 
       {times.map(time => (
-        <Time 
+        <Time
           key={time.nome}
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
-          colaboradores={colaboradores}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         />
       ))}
     </div>
